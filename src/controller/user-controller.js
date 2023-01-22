f9b1ec6283a62b26d59bc4671ac6750b7eb62592
“jinchunye” <1767874830@qq.com>
2023-01-22 21:37:37 +0800

    解析body数据，抽离数据库逻辑

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,6 +1,12 @@
+const { createUser } = require("../service/user.service");
 class Controller {
   async register(ctx, next) {
-    ctx.body = "这是用户页面";
+    // 1.获取数据
+    const { user_name, password } = ctx.request.body;
+    // 2.写入数据库
+    const res = await createUser(user_name, password);
+    // 3.返回结果
+    ctx.body = res;
   }
   async login(ctx, next) {
     ctx.body = "用户登陆";
